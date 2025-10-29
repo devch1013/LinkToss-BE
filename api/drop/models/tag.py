d5effe5873bdf013ccf3e07a5fd37e7546495ceb
ChanHyuk Park <devch1013@gmail.com>
2025-10-29 22:01:56 +0900

    tag mapping 모델 제작

--- api/drop/models/tag.py
+++ api/drop/models/tag.py
@@ -1,8 +1,18 @@
 from django.db import models
 
+from api.drop.models.drop import Drop
 from common.abstract_models.soft_delete_model import SoftDeleteModel
 from common.abstract_models.time_stamp_model import TimeStampModel
 
 
 class Tag(TimeStampModel, SoftDeleteModel):
     name = models.CharField(max_length=255)
+
+
+class TagDropMapping(TimeStampModel):
+    tag = models.ForeignKey(
+        Tag, on_delete=models.CASCADE, related_name="tag_drop_mappings"
+    )
+    drop = models.ForeignKey(
+        Drop, on_delete=models.CASCADE, related_name="tag_drop_mappings"
+    )
